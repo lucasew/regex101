22d040e7bf77612dd057bc0b5b1c9d5229f91bce
lucasew <lucas59356@gmail.com>
2024-07-26 21:43:28 -0300

    moving more logic to cmake
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- package.nix
+++ package.nix
@@ -3,9 +3,6 @@
 , qtwebengine
 , wrapQtAppsHook
 , cmake
-, gitMinimal
-, copyDesktopItems
-, makeDesktopItem
 }:
 
 stdenv.mkDerivation {
@@ -22,25 +19,6 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     wrapQtAppsHook
     cmake
-    gitMinimal
-    copyDesktopItems
-  ];
-
-  postInstall = ''
-    # copy icons
-    for res in 16 32 64 128 256 512 1024; do
-      res="${"$"}{res}x${"$"}{res}"
-      install -D ../assets/*.iconset/icon_$res.png $out/share/icons/hicolor/$res/apps/io.github.lucasew.regex101.png
-    done
-  '';
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "regex101.desktop";
-      desktopName = "Regex 101";
-      icon = "io.github.lucasew.regex101";
-      exec = "io.github.lucasew.regex101";
-    })
   ];
 
   meta.mainProgram = "io.github.lucasew.regex101";
